862dbf1389689daecd75218db655487ed0f55570
John Yuill <john@fig4.com>
2025-08-21 15:16:21 -0700

    building out more examples

--- pandas-01.py
+++ pandas-01.py
@@ -4,6 +4,32 @@
 
 import pandas as pd
 
-# random csv added to repo
+# 3 main options for data frames
+
+# 1. from a data dictionary
+# 2. from a csv file
+# 3. from a excel filelist
+
+# 1. create a dataframe from a dictionary
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie', 'Reggie'],
+    'Age': [25, 30, 35, 28],
+    'City': ['Vancouver', 'Calgary', 'Edmonton', 'Toronto']
+}
+# convert to dataframe
+my_df = pd.DataFrame(data)
+print(my_df)
+
+# save to csv
+my_df.to_csv('my_df.csv', index=False)
+
+# 2. import csv - random csv added to repo
 df = pd.read_csv('exp_cats_gpt.csv')
 print(df.head())
+
+# 3. import excel - random excel added to repo
+df_excel = pd.read_excel('expenses.xlsx', 
+            sheet_name = 'expenses_01', 
+            skiprows=2) # (skip first 2 rows)
+print(df_excel.head())
+
